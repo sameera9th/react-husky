e85954d5ddaea5791e2d21f1cc3724c05cc33504
Yasith Fernando <emmanuelyasith@gmail.com>
2019-11-27 14:17:35 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const count = 0;
+  const counts = 0;
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. {count}
+          Edit <code>src/App.js</code> and save to reload. {counts}
         </p>
         <a
           className="App-link"
